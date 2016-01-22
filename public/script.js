de30f1cabe5912076e126ffe0ac20ad5959d351a
tonsavenije <ton.savenije@gmail.com>
2016-01-22 10:01:27 +0100

    Add Redis and Save js

--- public/script.js
+++ public/script.js
@@ -4,14 +4,33 @@ window.onload = function() {
     var converter = new showdown.Converter();
     var pad = document.getElementById('pad');
     var markdownArea = document.getElementById('markdown');   
-
+	
+	var previousMarkdownValue;
+	
     var convertTextAreaToMarkdown = function(){
         var markdownText = pad.value;
+		previousMarkdownValue = markdownText;
         html = converter.makeHtml(markdownText);
         markdownArea.innerHTML = html;
     };
+	
+	var didChangeOccur = function() {
+		if(previousMarkdownValue != pad.value) {
+			return true;
+		}
+		return false;
+	}
+	
+	setInterval(function() {
+		if(didChangeOccur()) {
+			convertTextAreaToMarkdown();
+		}
+	}, 1000)
 
     pad.addEventListener('input', convertTextAreaToMarkdown);
-
-    convertTextAreaToMarkdown();
+	
+	sharejs.open('home', 'text', function(error, doc) {
+		doc.attach_textarea(pad);
+		convertTextAreaToMarkdown();
+	})
 };
